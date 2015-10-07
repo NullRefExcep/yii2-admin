3f8b7130330503335fde774ec2629911a439bfa2
Dmytro Karpovych <ZAYEC77@gmail.com>
2015-10-07 11:13:40 +0300

    add assets when install

--- src/Installer.php
+++ src/Installer.php
@@ -7,11 +7,24 @@
 namespace nullref\admin;
 
 use nullref\core\components\ModuleInstaller;
+use yii\helpers\Console;
 
 class Installer extends ModuleInstaller
 {
     public function getModuleId()
     {
         return 'admin';
+    }
+
+    public function install()
+    {
+        parent::install();
+        if (Console::confirm('Create assets files?')) {
+            $this->createFile('@webroot/js/admin/scripts.js');
+            echo 'File @webroot/js/admin/scripts.js was created' . PHP_EOL;
+
+            $this->createFile('@webroot/css/admin/main.css');
+            echo 'File @webroot/css/admin/main.css was created' . PHP_EOL;
+        }
     }
 }
